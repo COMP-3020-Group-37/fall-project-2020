f3edbd213193dedc3f8be2983a5ba4dddf0dbcd8
PetTurtle <ryhani@protonmail.com>
2020-11-24 23:11:15 -0600

    Added global variables

--- Website/script/database.js
+++ Website/script/database.js
@@ -6,6 +6,11 @@ import { Account } from './data_account.js';
 export class Database {
     constructor() {
 
+        // ----- Global Variables -----
+
+        this.yourLocation = null;
+        this.currentAccount = null;
+
         // ----- Set Restaurants -----
 
         this.restaurants = [
@@ -127,8 +132,6 @@ export class Database {
         this.accounts[0].favourite(this.restaurants[0]);
         this.accounts[0].favourite(this.restaurants[1]);
         this.accounts[0].favourite(this.restaurants[2]);
-
-        this.currentAccount = null;
     }
 
     getRestaurant(name) {
