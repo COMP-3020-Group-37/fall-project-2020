af173891e6a5ff3abbb5ebd353dcb48ae749ae4f
PetTurtle <ryhani@protonmail.com>
2020-11-26 00:30:35 -0600

    Hide search when not on home page

--- Website/script/state_cart.js
+++ Website/script/state_cart.js
@@ -3,14 +3,20 @@ import { State } from './state.js';
 export class StateCart extends State {
     constructor(wd, doc, db) {
         super('#cart', wd, doc, db);
+
+        this.sortHeader = doc.getElementById('search-header');
     }
 
     onEnter() {
 
+
+        this.sortHeader.style.display = 'none';
     }
 
     onExit() {
         this.main.innerHTML = '';
         this.sidebar.innerHTML = '';
+
+        this.sortHeader.style.display = '';
     }
 }
