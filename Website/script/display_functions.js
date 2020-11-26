43baadad009543bc33637b97c5310af1040cd9ad
Zac Kolton <koltonz@myumanitoba.ca>
2020-11-25 19:53:00 -0600

    edited js file

--- Website/script/display_functions.js
+++ Website/script/display_functions.js
@@ -1,5 +1,7 @@
 //
-// To clean up code in the html files, i added this js 
+// To clean up code in the html files, i added this js
+// Instead of writing statements like "document.getElementById('id').styles.display='block'
+// We can just create a function to call
 //
 
 function openSortMenu()
